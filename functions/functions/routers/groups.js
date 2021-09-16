42f702e15b82574d469c3a68e6c76e9514b72144
HuskyHsu <schi.hsu@gmail.com>
2021-09-16 23:21:23 +0800

    add group exist check

--- functions/functions/routers/groups.js
+++ functions/functions/routers/groups.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
 const router = express.Router();
+const db = admin.firestore();
 
 // create group
 router.post("/", async (req, res) => {
@@ -14,8 +15,16 @@ router.post("/", async (req, res) => {
     members: [req.user.user_id],
   };
 
-  const writeResult = await admin.firestore().collection("groups").add(data);
+  const groupsRef = db.collection("groups");
+  const allGroupRes = await groupsRef.where("name", "==", data.name).get();
+  if (!allGroupRes.empty) {
+    allGroupRes.forEach((doc) => {
+      functions.logger.warn(doc.id, "=>", doc.data());
+    });
+    return res.json({ result: `Group: ${data.name} exist` });
+  }
 
+  const writeResult = await groupsRef.add(data);
   res.json({ result: `Group with ID: ${writeResult.id} Created.` });
 });
 
