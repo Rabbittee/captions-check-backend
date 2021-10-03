c588fc7b4e3e4364333ea1e1de974479ae860032
HuskyHsu <schi.hsu@gmail.com>
2021-10-03 23:39:18 +0800

    test: 💍 add user test case

---
+++ functions/functions/test/index.test.js
@@ -0,0 +1,45 @@
+const admin = require("firebase-admin");
+const chai = require("chai");
+const sinon = require("sinon");
+
+const test = require("firebase-functions-test")({
+  projectId: "functions-test-6da7a",
+});
+const assert = chai.assert;
+
+describe("Cloud Functions", () => {
+  let myFunctions, adminInitStub;
+
+  before(() => {
+    adminInitStub = sinon.stub(admin, "initializeApp");
+    myFunctions = require("../index");
+  });
+
+  after(() => {
+    adminInitStub.restore();
+    test.cleanup();
+  });
+
+  describe("User", async () => {
+    it("tests an Auth function that interacts with Firestor", async () => {
+      const wrapped = test.wrap(myFunctions.createUser);
+
+      const uid = `${new Date().getTime()}`;
+      const email = `user-${uid}@example.com`;
+      const user = test.auth.makeUserRecord({
+        uid,
+        email,
+      });
+
+      // Call the function
+      await wrapped(user);
+
+      //   Check the data was written to the Firestore emulator
+      const snap = await admin.firestore().collection("users").doc(email).get();
+      const data = snap.data();
+
+      assert.equal(data.uid, uid);
+      assert.equal(data.email, email);
+    });
+  }).timeout(5000);
+});
