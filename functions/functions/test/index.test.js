e0bf7f23b15e0e846858b401522326414288f336
HuskyHsu <schi.hsu@gmail.com>
2021-10-04 23:57:14 +0800

    test: 💍 fix user create test

--- functions/functions/test/index.test.js
+++ functions/functions/test/index.test.js
@@ -1,43 +1,34 @@
 const admin = require("firebase-admin");
 const chai = require("chai");
-const sinon = require("sinon");
 
 const test = require("firebase-functions-test")();
 const assert = chai.assert;
+const myFunctions = require("../index");
 
 describe("Cloud Functions", () => {
-  let myFunctions, adminInitStub;
-
-  before(() => {
-    adminInitStub = sinon.stub(admin, "initializeApp");
-    myFunctions = require("../index");
-  });
-
   after(() => {
-    adminInitStub.restore();
     test.cleanup();
   });
 
-  describe("User", async () => {
-    it("tests an Auth function that interacts with Firestor", async () => {
-      const wrapped = test.wrap(myFunctions.createUser);
+  it("tests an Auth function that interacts with Firestore", async () => {
+    const wrapped = test.wrap(myFunctions.createUser);
 
-      const uid = `${new Date().getTime()}`;
-      const email = `user-${uid}@example.com`;
-      const user = test.auth.makeUserRecord({
-        uid,
-        email,
-      });
-
-      // Call the function
-      await wrapped(user);
-
-      //   Check the data was written to the Firestore emulator
-      const snap = await admin.firestore().collection("users").doc(email).get();
-      const data = snap.data();
-
-      assert.equal(data.uid, uid);
-      assert.equal(data.email, email);
+    const uid = `${new Date().getTime()}`;
+    const email = `user-${uid}@example.com`;
+    const user = test.auth.makeUserRecord({
+      providerData: [{ providerId: uid }],
+      uid,
+      email,
     });
-  }).timeout(5000);
+
+    // Call the function
+    await wrapped(user);
+
+    //   Check the data was written to the Firestore emulator
+    const snap = await admin.firestore().collection("users").doc(email).get();
+    const data = snap.data();
+
+    assert.equal(data.uid, uid);
+    assert.equal(data.email, email);
+  }).timeout(30000);
 });
